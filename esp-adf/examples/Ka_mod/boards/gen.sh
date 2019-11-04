cc018dd46e8553d0026c28933e4e5fdfbc56e0df
pilnikov <pilnikovav@yandex.ru>
2019-11-04 09:46:50 +0500

    update adf

--- esp-adf/examples/Ka_mod/boards/gen.sh
+++ esp-adf/examples/Ka_mod/boards/gen.sh
@@ -3,5 +3,5 @@ filename=$(basename "$1")
 fname="${filename%.*}"
 echo python ./gen.py ${fname}.csv build/${fname}.bin 0x2000
 python ./gen.py ${fname}.csv build/${fname}.bin 0x2000
-python /z/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port com5 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
+python ./esptool.py --chip esp32 --port com5 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
 echo done
